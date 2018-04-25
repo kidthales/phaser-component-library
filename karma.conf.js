b31632090b56eb21b478d723be0c25835c89ff1e
Tristan Bonsor <tristan.bonsor@gmail.com>
2018-04-24 22:28:49 -0700

    Update travis-ci config & revert Karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -27,13 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeNoSandbox'],
-    customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
