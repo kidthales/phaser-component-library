8131bf25768f0fe5f8894d8001e6e61c5a71498a
Tristan Bonsor <tristan.bonsor@gmail.com>
2018-04-26 20:33:11 -0700

    Update API documentation.

--- src/app/modules/phaser/phaser.component.ts
+++ src/app/modules/phaser/phaser.component.ts
@@ -1,7 +1,8 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
 /**
- * Phaser component. Instantiates a Phaser game instance and contains the corresponding HTMLCanvasElement.
+ * Phaser component. Instantiates a [Phaser game instance]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html} and contains
+ * the corresponding [HTMLCanvasElement]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html#canvas}.
  */
 @Component({
   // tslint:disable component-selector
@@ -10,22 +11,25 @@ import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, O
 })
 export class PhaserComponent implements AfterViewInit, OnDestroy, OnInit {
   /**
-   * Phaser game configuration input. Note that the 'parent' configuration property is ignored.
+   * [Phaser game configuration]{@link https://photonstorm.github.io/phaser3-docs/global.html#GameConfig} input. Note that the 'parent'
+   * configuration property is ignored.
    */
   @Input() public gameConfig: any;
 
   /**
-   * Game ready output - emits the Phaser game instance when it is ready and this component's view has initialized .
+   * Game ready output - emits the [Phaser game instance]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html} when it is
+   * ready and this component's view has initialized.
    */
   @Output() public readonly gameReady = new EventEmitter();
 
   /**
-   * Phaser module reference. This should provide access to the Phaser API.
+   * [Phaser]{@link https://photonstorm.github.io/phaser3-docs/Phaser.html} module reference. This should provide access to the Phaser API.
    */
   @Input() public Phaser: any;
 
   /**
-   * Phaser game reference. Instantiated in [ngOnInit]{@link PhaserComponent#ngOnInit}.
+   * [Phaser game]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html} reference. Instantiated in
+   * [ngOnInit]{@link PhaserComponent.ngOnInit}.
    */
   private game: any;
 
@@ -38,11 +42,11 @@ export class PhaserComponent implements AfterViewInit, OnDestroy, OnInit {
   public constructor(private elementRef: ElementRef) { }
 
   /**
-   * Lifecycle hook that is called after a component's view has been fully initialized. Attaches Phaser game's HTMLCanvasElement to DOM when
-   * game ready.
+   * Lifecycle hook that is called after a component's view has been fully initialized. Attaches
+   * [Phaser game's HTMLCanvasElement]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html#canvas} to DOM when game ready.
    */
   public ngAfterViewInit(): void {
-    // Possible race condition?
+    /** @todo Possible race condition? */
     this.game.events.once('ready', () => {
       this.elementRef.nativeElement.appendChild(this.game.canvas);
       this.elementRef.nativeElement.style.overflow = 'hidden';
@@ -51,7 +55,8 @@ export class PhaserComponent implements AfterViewInit, OnDestroy, OnInit {
   }
 
   /**
-   * Lifecycle hook that is called when a directive, pipe or service is destroyed. Destroys Phaser game instance.
+   * Lifecycle hook that is called when a directive, pipe or service is destroyed. Destroys
+   * [Phaser game]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html} instance.
    */
   public ngOnDestroy(): void {
     if (this.game && typeof this.game.destroy === 'function') {
@@ -60,7 +65,12 @@ export class PhaserComponent implements AfterViewInit, OnDestroy, OnInit {
   }
 
   /**
-   * Lifecycle hook that is called after data-bound properties of a directive are initialized. Instantiates Phaser game instance.
+   * Lifecycle hook that is called after data-bound properties of a directive are initialized. Instantiates
+   * [Phaser game]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html} instance.
+   *
+   * @throws [ReferenceError]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ReferenceError} if
+   * [Phaser]{@link https://photonstorm.github.io/phaser3-docs/Phaser.html} or
+   * [Phaser.Game]{@link https://photonstorm.github.io/phaser3-docs/Phaser.Game.html} are not available.
    */
   public ngOnInit(): void {
     // If Phaser module not provided try window object...
